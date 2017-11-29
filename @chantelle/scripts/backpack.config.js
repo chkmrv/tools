955277f7915f96532cd1f47b12d845c087f29ebc
Furkan Tunalı <mail@furkantunali.com>
2017-11-29 19:15:42 +0100

    clear config leftovers

--- @chantelle/scripts/backpack.config.js
+++ @chantelle/scripts/backpack.config.js
@@ -1,17 +1,7 @@
 /* eslint fp/no-mutation:0,import/no-commonjs:0 */
-const { debug } = require('@chantelle/util')
-const { DefinePlugin } = require('webpack')
 
-const webpack = ({
-  plugins: [
-    DefinePluginToDrop,
-    // ...plugins,
-  ], ...config
-}) => ({
-  plugins: [
-    // debug(new DefinePlugin({}), 'DefinePlugin %O'), //eslint-disable-line better/no-new
-    // ...plugins,
-  ],
+const webpack = ({ plugins: [DefinePluginToDrop, ...plugins], ...config }) => ({
+  plugins: [],
   ...config,
 })
 
